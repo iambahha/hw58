66fff0005a1dc879b9cf431fc4390b97aa938c1f
Baktiyar Ishenov <b1963826@gmail.com>
2019-12-11 22:31:38 +0600

    добавил App js и App css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+
+function App() {
+  return (
+    <BurgerBuilder />
+  );
+}
+
+export default App;
